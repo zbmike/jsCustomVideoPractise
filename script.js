d44540fdb54a9ae9bdf9c1da42f8e49a3c8e318e
Zhiyuan He <zh2312@columbia.edu>
2020-04-20 17:52:42 -0400

    Add setVIdeoProgress function to make progress bar work

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function updateProgress() {
 
 // Set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
